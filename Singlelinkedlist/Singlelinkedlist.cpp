d05d6e6b52b0b2aa72848d2ffad98cbe8677ab2c
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-04-25 11:05:20 +0700

    case 4&5

--- Singlelinkedlist/Singlelinkedlist.cpp
+++ Singlelinkedlist/Singlelinkedlist.cpp
@@ -131,40 +131,62 @@ void seacrhData() {
 
 int main() {
 	int pilihan; 
-	do 
+	do
 	{
-		try 
+		try
 		{
-			cout << "1. Tambah Data" << endl; 
-			cout << "2. Hapus Data" << endl; 
-			cout << "3. Tampilkan Data" << endl; 
-			cout << "4. Cari Data" << endl; 
-			cout << "5. Keluar" << endl; 
-			cout << "pilihan: "; 
-			cin >> pilihan; 
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "pilihan: ";
+			cin >> pilihan;
 			switch (pilihan)
 			{
-			case 1: 
-				addNode(); 
-				cout << "Data Berhasil Ditambahkan" << endl; 
-				system("pause"); 
-				system("cls"); 
-				break; 
-			case 2: 
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
 				if (listEmpty())
 				{
 					cout << "List Kosong" << endl;
 					system("pause");
 					system("cls");
 					break;
-				} 
+				}
 
 
-				int nim; 
-				cout << "Masukan NIM: "; 
-				cin >> nim; 
+				int nim;
+				cout << "Masukan NIM: ";
+				cin >> nim;
 				if (deleteNode(nim)) {
 					cout << "nim: " << " berhasil dihapus" << endl;
 					system("pause");
 					system("cls");
 				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				seacrhData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
+			}
+		}
+		catch (exception e)
+		{
+			cout << "Terjadi kesalahan" << endl;
+		}
+
+	} while (pilihan != 5); 
