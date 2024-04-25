886f84f22053e42598721e8e9ae7d93f16dc2563
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-04-25 10:15:29 +0700

    return true

--- Singlelinkedlist/Singlelinkedlist.cpp
+++ Singlelinkedlist/Singlelinkedlist.cpp
@@ -77,5 +77,9 @@ bool deleteNode(int nim) {
 	Node* previous = START; 
 	if (serachNode(nim, previous, current) == false)
 		return false; 
+	previous->next = current->next; 
+	if (current == START)
+		START = current->next; 
+	return true; 
 }
 
