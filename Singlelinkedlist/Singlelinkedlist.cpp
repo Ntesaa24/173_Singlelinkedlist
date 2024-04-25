6c82360cfb0a38a689c0f1a41364df79a06169aa
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-04-25 10:07:10 +0700

    current next

--- Singlelinkedlist/Singlelinkedlist.cpp
+++ Singlelinkedlist/Singlelinkedlist.cpp
@@ -55,7 +55,8 @@ bool serachNode(int nim, Node* current, Node* previous) {
 	current = START; 
 	while (current != NULL && nim > current->noMhs)
 	{
-
+		previous = current; 
+		current = current->next; 
 	}
 }
 
